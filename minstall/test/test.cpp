8b1084eeb4fcaed0974a78584c3a844d119f9e4a
liliassg <114520817+liliassg@users.noreply.github.com>
2025-12-25 02:14:33 +0100

    Upload test/test.cpp v1.0.0 by just_test

---
+++ minstall/test/test.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+namespace includecpp {
+std::string TEXT = "NULL";
+std::string UPLOADHASH = "NULL";
+std::vector<int> PrimaryInsert = {};
+std::vector<int> SecondaryInsert = {};
+std::vector<int> Insertion(){
+    //No Definition
+    return {};
+};
+}
